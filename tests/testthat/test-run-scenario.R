de88fb8db1cdcd19bb4f8e00e70fa168de6a8bba
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-10-30 14:41:13 -0400

    Add expectations to run-scenario test

--- tests/testthat/test-run-scenario.R
+++ tests/testthat/test-run-scenario.R
@@ -2,4 +2,7 @@ context("Running scenarios")
 
 test_that("Running basic scenario works", {
   out <- run_scenario("historical")
+  out_ca <- subset(out, variable == "Ca")
+  expect_true(all(out_ca$value > 200))
+  expect_equivalent(range(out$year), c(1765, 2014))
 })
