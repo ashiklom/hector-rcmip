5f75486df619d8f11b002235e55d962976491788
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-10-30 09:48:21 -0400

    Test that all emissions are zero

--- tests/testthat/test.basefile.R
+++ tests/testthat/test.basefile.R
@@ -9,4 +9,14 @@ test_that("Hector will run with default INI file", {
   result <- hector::fetchvars(hc, dates, hector::ATMOSPHERIC_CO2())
   expect_true(nrow(result) == length(dates))
   expect_true(all(result[["value"]] > 0))
+
+  # All emissions should be zero
+  emiss_vars <- sprintf("%s_emissions", c(
+    "ffi", "luc", "BC", "OC",
+    "SO2", "CH4",
+    #"NOX", "CO", "NMVOC",
+    "N2O", "CF4", "C2F6", "HFC4310", "HFC125"
+  ))
+  emissions <- hector::fetchvars(hc, dates, c(emiss_vars, "SV"))
+  expect_equivalent(sum(emissions$value ^ 2), 0)
 })
