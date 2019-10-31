2b37c77eefb0eacfdbcf5497d9ca55ed7dcd7785
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-10-31 14:59:29 -0400

    Add rcmip_outputs unit test

--- tests/testthat/test-run-scenario.R
+++ tests/testthat/test-run-scenario.R
@@ -1,12 +1,19 @@
 context("Running scenarios")
 
-test_that("Running basic scenario works", {
-  hc <- run_scenario("historical")
+test_that("Running historical scenario works", {
+  expect_warning(
+    hc <- run_scenario("historical"),
+    "Scenario historical has no data for Ftalbedo"
+  )
   start <- hector::startdate(hc)
-  expect_equal(start, 1765)
   end <- hector::enddate(hc)
-  expect_equal(end, 2014)
   out <- fetchvars2(hc, "Ca")
-  expect_equivalent(range(out$year), c(1765, 2014))
   expect_true(all(out$value > 200))
+
+  test_that("Reading outputs in RCMIP format works", {
+    rcout <- rcmip_outputs(hc)
+    expect_true("Atmospheric Concentrations|CO2" %in% rcout$variable)
+    expect_true("Carbon Sequestration" %in% rcout$variable)
+    expect_true("Heat Uptake|Ocean" %in% rcout$variable)
+  })
 })
