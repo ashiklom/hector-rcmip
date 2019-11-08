54cd15fda1bc2bb4a7b293d8bd42cdc668e085f5
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-11-08 14:34:46 -0500

    Add chemical unit conversion function

--- R/ud_convert2.R
+++ R/ud_convert2.R
@@ -1,11 +1,15 @@
 #' Chemical symbol-aware unit conversion
 #'
-#' @param x
-#' @param from
-#' @param to
-#' @return
+#' @param x Numeric value to convert
+#' @param from,to Units from/to which to convert. Syntax is identical to
+#'   [udunits2::ud.convert()] except that chemical symbols in hard brackets
+#'   (e.g. `[N2O]`) can be converted.
+#' @return Values of `x` converted to `to` units.
 #' @author Alexey Shiklomanov
 #' @export
+#' @examples
+#' ud_convert2(5, "Pg [C]", "Mt [CO2]")
+#' #> 18.32223
 ud_convert2 <- function(x, from, to) {
   udunits2::ud.convert(x, parse_chem(from), parse_chem(to))
 }
