b7d0fabd52356bffe6fe48b8abd7f10339e9204f
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-11-08 17:08:39 -0500

    Finalize tier 1 submission drake script

--- scripts/tier-1-scenarios.R
+++ scripts/tier-1-scenarios.R
@@ -1,13 +1,16 @@
 library(drake, exclude = c("gather", "expand"))
 library(tidyverse)
 library(here)
+library(glue, exclude = "collapse")
+library(fs)
 
 devtools::load_all(here())
 expose_imports("hector.rcmip")
 
 options(nwarnings = 150)
 
-dir.create("figures", showWarnings = FALSE)
+outdir <- dir_create(here("output"))
+figdir <- dir_create(here("figures"))
 
 scenarios <- c(
   "piControl", "esm-piControl", "1pctCO2", "1pctCO2-4xext",
@@ -48,6 +51,74 @@ plan <- drake_plan(
       strip.text.y = element_text(angle = 0),
       axis.text.x = element_text(angle = 90),
       legend.position = "bottom"
-    )
+    ),
+  scenario_df = rcmip_inputs() %>%
+    distinct(Model, Scenario, Region),
+  rcmip_vars = rcmip_inputs() %>%
+    distinct(Variable, Unit),
+  all_results_rcmip_format = all_results %>%
+    rename(Scenario = rcmip_scenario, Variable = variable) %>%
+    select(-scenario) %>%
+    filter(year >= 1850) %>%
+    pivot_wider(names_from = "year", values_from = "value") %>%
+    left_join(scenario_df, "Scenario") %>%
+    left_join(rcmip_vars, "Variable") %>%
+    left_join(distinct(meta_model, rcmip_scenario, cmip6_model, ClimateModel),
+              c("Scenario" = "rcmip_scenario", "cmip6_model")) %>%
+    select(ClimateModel, Model, Scenario, Region, Variable, Unit,
+           matches("[[:digit:]]{4}")) %>%
+    write_csv(file_out(!!path(outdir, "your_data.csv"))),
+  cmip_params_form = cmip6_params() %>%
+    mutate(
+      "Model Configuration Description" = glue(
+        "Hector calibrated against {model}. ",
+        "ECS = {S} degC, ",
+        "ocean heat diffusivity = {diff} cm2/s, ",
+        "volcanic scaling factor = {volscl}, ",
+        "aerosol scaling factor = {alpha}."
+      )
+    ) %>%
+    select(cmip6_model = model, ECS = S, `Model Configuration Description`) %>%
+    add_row(
+      cmip6_model = "default", ECS = 3.0,
+      `Model Configuration Description` = paste0(
+        "Hector defaults. ",
+        "ECS = 3.0 degC, ",
+        "ocean heat diffusivity = 2.3 cm2/s, ",
+        "volcanic scaling factor = 1.0, ",
+        "aerosol scaling factor = 1.0"
+      )
+    ),
+  meta_model = all_results %>%
+    distinct(rcmip_scenario, cmip6_model) %>%
+    left_join(cmip_params_form, "cmip6_model") %>%
+    mutate(
+      "Climate Model Name" = "hector",
+      "Climate Model Version" = substr(git2r::revparse_single("../hector", "HEAD")$sha, 0, 8),
+      "Climate Model Configuration Label" = if_else(
+        cmip6_model == "default",
+        "DEFAULT",
+        sprintf("CMIP6-%s-CALIB", cmip6_model)
+      ),
+      ClimateModel = paste(`Climate Model Name`, `Climate Model Version`,
+                           `Climate Model Configuration Label`,
+                           sep = "|"),
+      "Project" = "",
+      "Name of Person" = "Alexey Shiklomanov",
+      "Literature Reference" = "https://doi.org/10.5194/gmd-8-939-2015"
+    ),
+  meta_model_write = meta_model %>%
+    distinct(
+      ClimateModel,
+      `Climate Model Name`,
+      `Climate Model Version`,
+      `Climate Model Configuration Label`,
+      `ECS`,
+      `Model Configuration Description`,
+      `Project`,
+      `Name of Person`,
+      `Literature Reference`
+    ) %>%
+    write_tsv(file_out(!!path(outdir, "meta_model.tsv")))
 )
 make(plan)
