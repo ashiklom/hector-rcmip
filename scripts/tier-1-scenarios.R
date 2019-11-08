5a1398400a7586c15d5e08f8fd154b4d18934bb2
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-11-08 11:46:56 -0500

    Use drake for tier 1 simulations

--- scripts/tier-1-scenarios.R
+++ scripts/tier-1-scenarios.R
@@ -1,11 +1,13 @@
+library(drake, exclude = c("gather", "expand"))
 library(hector.rcmip)
 library(tidyverse)
+library(here)
 
 options(nwarnings = 150)
 
 dir.create("figures", showWarnings = FALSE)
 
-tier1 <- tibble(Scenario = c(
+scenarios <- c(
   "piControl",
   "esm-piControl",
   "1pctCO2",
@@ -19,36 +21,27 @@ tier1 <- tibble(Scenario = c(
   ## "esm-hist",
   ## "esm-ssp119",
   ## "esm-ssp585"
-))
+)
 
-out <- list()
-for (i in seq_len(nrow(tier1))) {
-  s <- tier1[i, ][[1]]
-  tryCatch(
-    out[[s]] <- run_scenario(s),
-    error = function(e) {
-      message("Scenario ", s, " failed to run with error:\n",
-              conditionMessage(e))
-    }
-  )
+do_scenario <- function(scenario) {
+  core <- run_scenario(scenario)
+  rcmip_outputs(core, dates = 1750:2100)
 }
 
-results <- map_dfr(out, rcmip_outputs, dates = seq(1750, 2100))
-rplot(results)
-ggsave("figures/tier1-results-rcmip-vars.png", width = 10, height = 8)
-
-results %>%
-  filter(variable == "Carbon Sequestration",
-         year > 1845, year < 1880) %>%
-  ggplot() +
-  aes(x = year, y = value, color = scenario) +
-  geom_line()
-
-rplot(results) +
-  geom_hline(yintercept = 0, linetype = "dashed")
-
-core <- out[[1]]
-results_rcmip <- map_dfr(out, rcmip_outputs)
-
-ggsave("figures/tier1-results.png", width = 7, height = 7)
-
+plan <- drake_plan(
+  out = target(
+    do_scenario(scenario),
+    transform = map(scenario = !!scenarios)
+  ),
+  all_results = target(
+    bind_rows(out),
+    transform = combine(out)
+  ),
+  all_scenario_plot = ggplot(all_results) +
+    aes(x = year, y = value, color = scenario) +
+    geom_line() +
+    facet_wrap(vars(variable), scales = "free_y") +
+    scale_color_viridis_d() +
+    theme_bw()
+)
+make(plan)
