8fb96996a092646c3af4b3690a23891ce60fe345
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-11-11 11:40:15 -0500

    Add `quiet` as argument to fetchvars2

--- R/fetchvars2.R
+++ R/fetchvars2.R
@@ -2,18 +2,19 @@
 #' end dates
 #'
 #' @inherit hector::fetchvars params return
-#' @param dates
+#' @inherit purrr::possibly params
+#' @param quiet
 #' @param ... Additional arguments to [hector::fetchvars()]
 #' @author Alexey Shiklomanov
 #' @export
-fetchvars2 <- function(core, vars = NULL, dates = NULL, ...) {
+fetchvars2 <- function(core, vars = NULL, dates = NULL, quiet = FALSE, ...) {
   if (is.null(dates)) {
     dates <- seq(hector::startdate(core), hector::enddate(core))
   }
   if (!is.null(vars)) {
     purrr::map_dfr(
       vars,
-      purrr::possibly(hector::fetchvars, NULL),
+      purrr::possibly(hector::fetchvars, NULL, quiet = quiet),
       core = core,
       dates = dates,
       ...
