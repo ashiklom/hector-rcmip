8d7542637b29c49146c500b242871b6fa4c2fbf9
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-01-30 09:56:12 -0500

    Better use of drake targets in run-simulations

--- scripts/01-run-simulations.R
+++ scripts/01-run-simulations.R
@@ -31,6 +31,27 @@ models <- c(cmip6_params()[["model"]], "default")
 rcmip_infile <- here("inst", "rcmip-inputs.fst")
 if (!file.exists(rcmip_infile)) generate_rcmip_inputs()
 
+read_probability_scenario <- function(scenario_files) {
+  .datatable.aware <- TRUE
+  outdir = path(
+    "output", "zz-raw-output",
+    "probability-processed"
+  )
+  fs::dir_create(outdir)
+  dat <- rbindlist(lapply(scenario_files, fread))
+  setDT(dat)
+  scenario <- unique(dat[["scenario"]])
+  values <- dat[, .(year, variable, value)]
+  scenariofile <- fs::path(outdir, paste0(scenario, ".fst"))
+  fst::write_fst(values, scenariofile)
+  rm(values)
+  params <- unique(dat[, .(isamp, param_ecs, param_diffusivity, param_volscl)])
+  rm(dat)
+  paramfile <- fs::path(outdir, paste0("params.", scenario, ".fst"))
+  fst::write_fst(params, paramfile)
+  c(scenariofile, paramfile)
+}
+
 # Do the runs
 plan <- drake_plan(
   # Individual runs
@@ -44,6 +65,7 @@ plan <- drake_plan(
   ), col_types = cols(.default = "d")),
   isamps = sample.int(nrow(probability_params), 1000),
   probability_param_draws = probability_params[isamps, ],
+  dscenarios = scenarios,
   probability_run = target(
     run_with_param(
       scenario,
@@ -53,56 +75,16 @@ plan <- drake_plan(
       isamp = isamps
     ),
     dynamic = map(probability_param_draws, isamps),
-    transform = map(scenario = !!scenarios)
-  )
-)
-
-# Write the outputs into a concise format
-get_probability_files <- function(outdir) {
-  tibble::tibble(
-    directory = fs::dir_ls(outdir),
-    files = purrr::map(directory, dir_ls, glob = "*.csv") %>%
-      purrr::map(as.character)
-  ) %>%
-    tidyr::unnest(files) %>%
-    dplyr::transmute(
-      scenario = fs::path_file(directory),
-      file = files
-    )
-}
-
-read_probability_scenario <- function(scenario_files) {
-  .datatable.aware <- TRUE
-  outdir = path(
-    "output", "zz-raw-output",
-    "probability-processed"
-  )
-  fs::dir_create(outdir)
-  dat <- rbindlist(lapply(scenario_files[["file"]], fread))
-  setDT(dat)
-  scenario <- unique(dat[["scenario"]])
-  values <- dat[, .(year, variable, value)]
-  fst::write_fst(values, fs::path(outdir, paste0(scenario, ".fst")))
-  rm(values)
-  params <- unique(dat[, .(isamp, param_ecs, param_diffusivity, param_volscl)])
-  rm(dat)
-  fst::write_fst(params, fs::path(outdir, paste0("params.", scenario, ".fst")))
-}
-
-plan <- bind_plans(plan, drake_plan(
-  probability_files_df = get_probability_files(path(
-    outdir, "zz-raw-output", "probability"
-  )),
+    transform = map(scenario = !!scenarios, .id = scenario)
+  ),
   probability_processed = target(
-    read_probability_scenario(dplyr::filter(
-      probability_files_df, scenario == s
-    )),
-    transform = map(s = !!scenarios),
+    read_probability_scenario(probability_run),
+    transform = map(probability_run),
     # Without this, we run out of memory when trying to read multiple files
     # Might be able to get around it on beefier systems
     hpc = FALSE
   )
-))
+)
 
 ### Make plan
 options(clustermq.scheduler = "multicore")
