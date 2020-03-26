1c8deaeaeecd6d95998f3dd1c7163df4708caa68
Kalyn <kalyn.dorheim@pnnl.gov>
2020-03-26 17:47:23 -0400

    Update the variable conversion table to properly convert from N2O to N2O-Nadd tests to make ensure temp consistency with default rcps and check the n2o conversion.

--- tests/testthat/test-run-scenario.R
+++ tests/testthat/test-run-scenario.R
@@ -16,3 +16,32 @@ test_that("Running historical scenario works", {
     expect_true("Heat Uptake|Ocean" %in% rcout$variable)
   })
 })
+
+
+test_that("Cores set up properly",{
+  
+  # Compare one of the RCMIP RCP runs with RCP output from one of the 
+  # default Hector RCP emissions.
+  # RCMIP RCP RUN
+  rcmip <- read.csv(run_scenario('rcp45'), stringsAsFactors = FALSE)
+  rcmip$'rcmip' <- rcmip$value
+  rcmip <- rcmip[ , names(rcmip) %in% c('year', 'variable', 'rcmip')]
+  
+  # Hector RCP Run
+  core <- hector::newcore(system.file('input/hector_rcp45.ini', package = 'hector'))
+  hector::run(core)
+  true <- hector::fetchvars(core, 1850:2100, hector::GLOBAL_TEMP())
+  true$'hector' <- true$value
+  true <- true[ , names(true) %in% c('year', 'variable', 'hector')]
+
+  # Format the comparion data. 
+  comp_data      <- dplyr::left_join(rcmip, true)
+  temp_comp_data <- na.omit(comp_data[comp_data$variable == hector::GLOBAL_TEMP(), ])
+  temp_RMSE <- mean((temp_comp_data$rcmip - temp_comp_data$hector)^2)
+  
+  # Make sure that the results are within each other for some threshold. 
+  # Make sure that the temp results are within 0.01 deg C of one anohter. 
+  temp_threshold <- 0.01
+  testthat::expect_true(temp_RMSE < temp_threshold)
+  
+})
